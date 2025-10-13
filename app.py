4553669a563f966ac2d35750a7c99d2d13f5db6b
Edson <edson.alvaradomz@gmail.com>
2025-10-12 22:21:43 -0600

    se elimina carga de dataframe

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-car_data = pd.read_csv(
-    'C:\\Users\\edson\\AppData\\Local\\Programs\\Python\\Python313\\Python_Edson\\TripleTen\\Sprint_7\\Project_Sprit_7\\vehicles_us.csv')  # leer los datos
-print(car_data.columns)
+# car_data = pd.read_csv(
+#   'C:\\Users\\edson\\AppData\\Local\\Programs\\Python\\Python313\\Python_Edson\\TripleTen\\Sprint_7\\Project_Sprit_7\\vehicles_us.csv')  # leer los datos
+# print(car_data.columns)
 
 
 st.header('Lanzar una moneda')
